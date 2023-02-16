bae7d7211644b92488e9acc2ac402df0aa22f3b0
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-16 14:51:09 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Code start
+ * Description: using printf
+ * Return: Code end
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
