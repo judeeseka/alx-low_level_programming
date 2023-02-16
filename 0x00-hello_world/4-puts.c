dbc1f6946927741e3e7d7a97f9a30e7f4a625bb5
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-16 14:40:43 -0800

    fixed betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * using puts function
+ * main - Function start
+ * Description: using puts function
+ * Return: Function end
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
