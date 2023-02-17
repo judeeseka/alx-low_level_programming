23226aaa29b31d99cfd828882d8ff9e54ab9ee35
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-17 10:49:03 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+	}
+	putchar('\n');
+	return (0);
+}
