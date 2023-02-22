0c6851cdb158df5ab015c3ba1e5b2bd54225677c
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-22 12:31:07 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @x: first parameter
+ * @y: second parameter
+ * Return: an int value
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
