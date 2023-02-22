6b468cc1ee31661fff42e83dcc0f86b3861199aa
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-22 12:24:25 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (b == 0)
+			{
+				_putchar('0');
+			}
+			else if (c < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
