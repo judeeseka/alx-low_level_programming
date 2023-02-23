78d2ec32820bc8f52f2654a89df49c620dade0e0
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-23 06:37:00 -0800

    test positive or negative integers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * positive_or_negative - Entry point
- * @i: parameter
- * Return: void
+ * main - Entry point
+ * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-		return;
-	}
-}	
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
