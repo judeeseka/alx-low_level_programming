c19daea3deca4875c9c788dd5a53fee081d288e8
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-23 05:23:37 -0800

    testing out task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Entry point
+ * @i: parameter
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+{
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+		return;
+	}
+}	
