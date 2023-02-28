6cac56ea5f08813ba5f0953d6c4bdaac7cdb2e78
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-28 12:46:11 -0800

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: first parameter
+ * @b: second parameter
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
