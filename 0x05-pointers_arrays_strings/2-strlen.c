706a6fb32dd437767d0e4e5872b88fb8a5781c8d
Eseka Chinedu <esekachinedu256@gmail.com>
2023-02-28 13:04:59 -0800

    calc length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: parameter
+ * Return: an int value
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}
