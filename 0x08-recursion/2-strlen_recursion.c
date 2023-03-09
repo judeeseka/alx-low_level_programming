d48956acd6762c2885d6d2e19a1bbe0fac2aaf37
Eseka Chinedu <esekachinedu256@gmail.com>
2023-03-09 13:42:16 -0800

    print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - find the length of a string
+ * @s: string param
+ * Return: an int value
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len = len + _strlen_recursion(s + 1);
+	}
+
+	return (len)
+}
