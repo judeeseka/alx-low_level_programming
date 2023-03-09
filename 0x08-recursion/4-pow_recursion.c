e9116660e46d9ce48f29f737786d233eda345e1c
Eseka Chinedu <esekachinedu256@gmail.com>
2023-03-09 14:07:35 -0800

    value of x power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - find value of x to the power of y
+ * @x: the base value
+ * @y: the power value
+ * Return: an int value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
