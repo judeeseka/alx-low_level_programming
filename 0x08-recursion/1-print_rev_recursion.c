7f2b59c4975ec57f42b9fe51efd3dd7b806f179f
Eseka Chinedu <esekachinedu256@gmail.com>
2023-03-09 13:34:50 -0800

    fixed code output

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,12 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 
 	return (len);
