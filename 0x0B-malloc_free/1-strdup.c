51080ef25cf661372335de6c7775b5bb65279fb2
Eseka Chinedu <esekachinedu256@gmail.com>
2023-03-15 12:27:17 -0700

    made changes to output

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,10 +13,13 @@ char *_strdup(char *str)
 	char *p;
 	int len;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	p = malloc(sizeof(char) * len);
+	p = malloc(sizeof(char) * len + 1);
 
 	if (p == NULL)
 		return (NULL);
