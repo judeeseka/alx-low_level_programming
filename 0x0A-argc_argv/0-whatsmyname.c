561477f754c3f3e2e39d9b02c005bf4754ce5883
Eseka Chinedu <esekachinedu256@gmail.com>
2023-03-15 17:22:50 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print program name
+ * @argc: number of arguments
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
